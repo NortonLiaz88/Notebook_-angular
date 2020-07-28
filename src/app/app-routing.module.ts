b3281843921c27282d4514ab4685f6dc1ae2d658
Norton Liaz <vitornortonlimaazevedo@gmail.com>
2020-07-28 17:42:15 -0300

    primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
 
+//https://www.techiediaries.com/angular-lazy-load-module-example/
+const routes: Routes = [
+  //{ path: 'entries', loadChildren: () => import ('./pages/entries/entries.module').then(m => m.EntriesModule)},
+  { path: 'categories', loadChildren: () => import ('./pages/categories/categories.module').then(m => m.CategoriesModule) }
+];
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
